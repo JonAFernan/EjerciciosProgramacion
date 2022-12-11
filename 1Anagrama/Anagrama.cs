9d734ebec9801389a5d945a16c642ad5ce49ad00
JonAFernan <jonanfernandiaz@gmail.com>
2022-12-11 19:39:37 +0100

    Anadir enunciado ejercicio Anagrama

--- 1Anagrama/Anagrama.cs
+++ 1Anagrama/Anagrama.cs
@@ -1,4 +1,14 @@
-﻿public class Program
+﻿/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+
+public class Program
 {
     public static void Main(string[] args)
     {
