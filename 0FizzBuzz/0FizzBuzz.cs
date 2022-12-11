6eccf0ab73e094f8dca672d13987a39f8d7d7453
JonAFernan <jonanfernandiaz@gmail.com>
2022-12-11 19:08:42 +0100

    Subir ejercicios a github

---
+++ 0FizzBuzz/0FizzBuzz.cs
@@ -0,0 +1,9 @@
+﻿
+
+for(int i= 0 ; i<=100 ; i++)
+    {
+        if(i%3==0 && i%5==0 ) Console.WriteLine("fizzBuzz");
+        else if (i%3==0) Console.WriteLine("fizz");
+        else if (i%5==0) Console.WriteLine("buzz");
+        else Console.WriteLine(i);
+    }
---
+++ 1Anagrama/Anagrama.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string FirstWord = "veranillo";
+        string SecondWord = "lloverian";
+
+        Console.WriteLine(Anagrama(FirstWord, SecondWord));
+
+        static bool Anagrama(string word1, string word2)
+        {
+            bool result = false;
+            List<char> listChar = new List<char>();
+            if (word1 == word2) result = false;
+            else if (word1.Length != word2.Length) result = false;
+            else
+            {
+                List<char> listWord1 = new List<char>(word1);
+                List<char> listWord2 = new List<char>(word2);
+                foreach (var letter in listWord1)
+                {
+                    if (listWord2.Contains(letter))
+                    {
+                        listChar.Add(letter);
+                        listWord2.Remove(letter);
+                    }
+
+                }
+            }
+            string word3 = string.Join("", listChar);
+            if (word3 == word1) result = true;
+            return result;
+        }
+    }
+}
