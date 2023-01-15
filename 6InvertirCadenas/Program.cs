3338e5bde1b8bc17bb73352c4b8c1288dcc7158e
Jon A.Fernadez <jafernandiaz@gmail.com>
2023-01-15 11:43:20 +0100

    Ejercicio 6

---
+++ 6InvertirCadenas/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Crea un programa que invierta el orden de una cadena de texto
+ * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ */
+
+ 
+ 
+ ReverseString("hello world");
+ 
+ void ReverseString(string word)
+ {
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        System.Console.Write(word[i]);
+    }
+
+ }
