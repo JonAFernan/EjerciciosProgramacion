b9b2fe6fef3c139f937a6ffb06c9e96ab919c122
Jon A.Fernadez <jafernandiaz@gmail.com>
2023-01-22 01:33:37 +0100

    Enunciado ejercicio 8

---
+++ 8DecimalBinario/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _8DecimalBinario;
+
+/*
+ * Crea un programa se encargue de transformar un número
+ * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ */
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConvertToBinary(12);
+    }
+
+    static void ConvertToBinary(int number)
+    {
+        Console.WriteLine("Binary number");
+    }
+}
