2f9775996c209ffc526456e446bcd97f50462f19
Jon A.Fernadez <jafernandiaz@gmail.com>
2023-01-22 20:56:52 +0100

    Ejercicio 8 finalizado

--- 8DecimalBinario/Program.cs
+++ 8DecimalBinario/Program.cs
@@ -10,11 +10,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        ConvertToBinary(12);
+        ConvertToBinary(100);
     }
 
     static void ConvertToBinary(int number)
     {
-        Console.WriteLine("Binary number");
+        string binaryNumber="";
+        bool x= true;
+
+        if (number== 0) Console.WriteLine("0");
+        else if(number == 1) Console.WriteLine("1");
+        else 
+        {
+            do
+            {
+                binaryNumber += (number%2).ToString();
+                number = number/2;
+
+                if(number == 1) 
+                {
+                binaryNumber += number;    
+                x= false;
+                }
+            } while (x);
+
+            for (int i = binaryNumber.Length-1; 0 <= i; i--)
+            {
+                Console.Write(binaryNumber[i]);
+            }
+        
+        }  
+    
     }
 }
