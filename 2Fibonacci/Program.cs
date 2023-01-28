75b246f3acb13d0100299c1bb72dfb53e6b87e9d
JonAFernan <jonanfernandiaz@gmail.com>
2023-01-28 09:23:26 +0100

    Mejora del ejercicio

--- 2Fibonacci/Program.cs
+++ 2Fibonacci/Program.cs
@@ -1,30 +1,37 @@
-﻿/*
+﻿namespace Fibonacci;
+/*
  * Escribe un programa que imprima los 50 primeros números de la sucesión
  * de Fibonacci empezando en 0.
  * - La serie Fibonacci se compone por una sucesión de números en
  *   la que el siguiente siempre es la suma de los dos anteriores.
  *   0, 1, 1, 2, 3, 5, 8, 13...
  */
-
-uint [] FibonacciList = Fibonacci(50);
-foreach (var i in FibonacciList)
-{
-    Console.WriteLine(i);
-}
-
- 
-
-static uint [] Fibonacci(int numOfFibonums)
-{   
-    uint [] FiboList = new uint[numOfFibonums];
-    FiboList[0] = 0;
-    FiboList[1] = 1;
-
-    for (int index = 0 ; index < (FiboList.Length -2) ; index++)
-
-    {  
-        FiboList[index + 2] = FiboList[index] + FiboList[index + 1];
+class Program{
+ static void Main(string[] args)
+    {
+        Fibonacci(50);
     }
-        
-    return FiboList;
+
+static void Fibonacci(int numOfFibonums)
+{   
+    
+    long firstNum = 0;
+    long secondNum = 1;
+    long sumFirstSecond;
+    System.Console.WriteLine(firstNum);
+
+    if(numOfFibonums > 1)
+    {
+        System.Console.WriteLine(secondNum);
+
+        for (int x= 2 ; x < numOfFibonums ; x++)
+
+        {  
+            sumFirstSecond = firstNum + secondNum;
+            Console.WriteLine(sumFirstSecond);
+            firstNum = secondNum;
+            secondNum = sumFirstSecond;
+        }
+    }
 }   
+}
