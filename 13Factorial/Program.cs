24dd8dd28698c9e592169dd170439d752b71707b
Jon A.Fernadez <jafernandiaz@gmail.com>
2023-02-11 11:36:59 +0100

    Ejercicio finalizado

---
+++ 13Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _13Factorial;
+
+/*
+ * Escribe una función que calcule y retorne el factorial de un número dado
+ * de forma recursiva.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Factorial(4));
+    }
+
+    static int Factorial(int number)
+    {
+        if(number == 1) return number;
+        return number * Factorial(number-1);
+    }
+}
