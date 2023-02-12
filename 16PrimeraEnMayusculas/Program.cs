28f36149bc7d42df4c84fa290ab8b11c44e8f237
Jon A.Fernadez <jafernandiaz@gmail.com>
2023-02-12 09:33:11 +0100

    Ejercicio 16 finalizado y añadido ejercicio 15

---
+++ 16PrimeraEnMayusculas/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _16PrimeraEnMayusculas;
+
+/*
+ * Crea una función que reciba un String de cualquier tipo y se encargue de
+ * poner en mayúscula la primera letra de cada palabra.
+ * - No se pueden utilizar operaciones del lenguaje que
+ *   lo resuelvan directamente.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FirstToMayus("¡hello!"));
+        Console.WriteLine(FirstToMayus("hello"));
+        Console.WriteLine(FirstToMayus("Hello"));
+    }
+
+    static string FirstToMayus(string word)
+    {
+        string firstLetterMayus = "";
+        int mayusCounter = 0;
+        char [] letters = "abcdefghijklmnopqrstuvwxyz".ToArray();
+        word = word.ToLower();
+        for (int i = 0; i < word.Length; i++)
+        {
+            if(mayusCounter == 0 && (word[i].ToString()).IndexOfAny(letters) != -1)
+            {
+                firstLetterMayus += (word[i].ToString()).ToUpper();
+                mayusCounter ++;
+            }
+            else firstLetterMayus += word[i];
+        }
+
+
+        return firstLetterMayus;
+    }
+}
