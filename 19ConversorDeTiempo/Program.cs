e60f68a5581f8db1072fcd93c3f7f837550d11cf
Jon A.Fernadez <jafernandiaz@gmail.com>
2023-03-04 01:45:24 +0100

    Ejercicio 19 finalizado

---
+++ 19ConversorDeTiempo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _19ConversorDeTiempo;
+
+/*
+ * Crea una función que reciba días, horas, minutos y segundos (como enteros)
+ * y retorne su resultado en milisegundos.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(TimeToMiliseconds(2147483647,2147483647,2147483647,2147483647));
+    }
+
+    static ulong TimeToMiliseconds( int days ,int hours, int minutes, int seconds)
+    {
+        return  ((ulong)days * 86400000 + (ulong)hours * (ulong)3600000 + (ulong)minutes * 60000 + (ulong)seconds * 1000);
+    }
+    
+}
